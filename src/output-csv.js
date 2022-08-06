a2c90978c26cc7cd03e50906b62d38aaca6725a5
Naoki Takahashi <nemuki@users.noreply.github.com>
2022-08-06 16:10:54 +0900

    output-csv.jsを追加

---
+++ src/output-csv.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const outputCSV = (groupData) => {
+    let outputText = "Pre-assign Room Name,Email Address\n";
+    let groupId = 1;
+
+    for (let i = 0; i < groupData.length; i++) {
+        for (let j = 0; j < groupData[i].length; j++) {
+            outputText += "グループ" + groupId + "," + groupData[i][j] + "\n";
+        }
+        groupId++;
+    }
+
+    const blob = new Blob([outputText], {type: "text/plain"});
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    document.body.appendChild(a);
+    a.download = "output.csv";
+    a.href = url;
+    a.click();
+    a.remove();
+    URL.revokeObjectURL(url);
+}
